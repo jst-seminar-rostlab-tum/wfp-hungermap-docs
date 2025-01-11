b54988cbe527ea2053d9f9d305260708dad35b5a
Sámuel Fekete <feketesamu@gmail.com>
2025-01-11 20:26:16 +0100

    feat: remove frontpage

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,19 +6,14 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 const config: Config = {
   title: "WFP HungerMap documentation",
-  tagline: "Dinosaurs are cool",
+  tagline: "",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://docs-wfp-hungermap.netlify.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
