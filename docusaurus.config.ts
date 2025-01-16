f8f676b027a8a477f91ef5178b0b27da4d47512c
Sámuel Fekete <feketesamu@gmail.com>
2025-01-16 12:32:26 +0100

    fix: footer, category desc

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -121,7 +121,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} ??? Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Center for Software Engineering Excellence GmbH - Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
