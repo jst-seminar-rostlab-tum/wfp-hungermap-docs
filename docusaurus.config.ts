8c96c0f0279c6f57964368e17267665ab9f4cd91
Muhammed Emre Bayraktaroglu <69143179+memreo@users.noreply.github.com>
2025-01-20 13:36:11 +0100

    feat: Add docusaurus-plugin-image-zoom for enhanced image zooming and create overview of dependencies documentation (#42)
    * fix: Update HTML line breaks in service configuration documentation
    
    * fix: Correct links in chatbot documentation for database structure and API specifications
    
    * fix: move util docs in to `Utilities` (#41)
    
    merge with PR #40
    
    * docs: Add author information to run_all_uploaders.md
    
    * feat: Add docusaurus-plugin-image-zoom for enhanced image zooming and create overview of dependencies documentation
    
    * docs: Update section headers in Overview of Dependencies for consistency

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,9 +44,23 @@ const config: Config = {
     ],
   ],
 
+  plugins: [
+    'docusaurus-plugin-image-zoom',
+  ],
+
   themeConfig: {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
+    zoom: {
+      selector: '.markdown :not(em) > img',
+      background: {
+        light: 'rgb(255, 255, 255)',
+        dark: 'rgb(50, 50, 50)'
+      },
+      config: {
+        // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
+      }
+    },
     navbar: {
       title: "WFP HungerMap",
       logo: {
